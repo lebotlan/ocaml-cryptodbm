e098b127dabc07b787706012e557d65f2fe69d6c
Le Botlan <contact.lebotlan@insa-toulouse.fr>
2017-07-09 16:49:31 +0200

    Examples

--- examples/ex_write.ml
+++ examples/ex_write.ml
@@ -22,24 +22,13 @@ let write_file () =
   add subt2 ~key:"public-info1" ~data:"public-data1" ;
 
   close table ;
-  ()
 
-
-let read_file () =
-
-  let table = open_read ~file ~passwd ~signwd:"" in
-
-  let subt1 = open_subtable table ~name:"my subtable #1" ~passwd:"" ~signwd:"" () in
-  iter subt1 (fun key data -> Printf.printf " Key = %s  Data = %s\n" key data) ;
-
-  let subt2 = open_uncrypted_subtable table ~name:"a public subtable" ~signwd:"" () in
-  iter subt2 (fun key data -> Printf.printf " Key = %s  Data = %s\n" key data) ;
+  Printf.printf "File %s written\n%!" file ;
     
-  close table ;
   ()
 
 
-let () = read_file ()
+let () = write_file ()
 
 (* You can browse the generated db file using test/dbmreader.exe :
 
